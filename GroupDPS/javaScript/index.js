af38bbfaeb46a702695df96d262de0b1496f538a
aeliyadevs <aeliyadevs@gmail.com>
2024-07-31 22:47:45 +0545

    group-dps: javaScript variables and datatypes (string, number, boolean, null, undefined, array)

---
+++ GroupDPS/javaScript/index.js
@@ -0,0 +1,42 @@
+// Single line comment
+
+/*
+Multi-line comment
+this is my
+ multi-line
+ comment 
+ */
+
+//  Variables
+// let
+let a = 5;
+a = 50;
+// const
+const PI = 3.14;
+// var
+var b = 9;
+b = 90;
+console.log(a);
+console.log(b);
+
+// Datatypes
+// string
+let firstname = "Aeliya";
+console.log(firstname);
+
+// number
+let age = 100;
+
+// boolean
+let off = true;
+
+// null and undefined
+let x;
+let y = "";
+console.log(x);
+console.log(y);
+
+// array - collection of similar data(datatypes)
+var fruits = ["apple", "jackfruit", "banana", "mango"];
+var subject = ["math", "science", "computer"];
+var prime = [1, 3, 5, 7, 11, 13, 17];
