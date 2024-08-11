6d84686433561f6adb96732d3961be8d72a12ae8
aeliyadevs <aeliyadevs@gmail.com>
2024-08-11 22:48:07 +0545

    grou-dps: js - conditional statement - if - else if - else

---
+++ GroupDPS/javaScript/conditional.js
@@ -0,0 +1,31 @@
+// Statement
+let percentage = 25;
+
+// Conditional Statement
+// if
+if (percentage > 40) {
+  // things to execute when the condition is true
+  console.log("Passed");
+}
+
+// if else
+if (percentage > 40) {
+  // things to execute when the condition is true
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
+
+// if - else if - else
+let GPA = 3.7;
+if (GPA > 3.6 && GPA <= 4) {
+  console.log("A+");
+} else if (GPA > 3.2 && GPA <= 3.6) {
+  console.log("A");
+}
+// else if (condition3) {
+//   //
+// }
+else {
+  console.log("Invalid GPA value.");
+}
