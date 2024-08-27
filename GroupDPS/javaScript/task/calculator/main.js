0a1375ea9245cfba60ab65c7068cdb3003cd18f6
aeliyadevs <aeliyadevs@gmail.com>
2024-08-27 20:02:03 +0545

    group-dps: calculator project - implement subtraction, multiplication and division

--- GroupDPS/javaScript/task/calculator/main.js
+++ GroupDPS/javaScript/task/calculator/main.js
@@ -14,6 +14,9 @@ let x, y;
 let firstInput = document.getElementById("num1");
 let secondInput = document.getElementById("num2");
 let addBtn = document.getElementById("add");
+let subtractBtn = document.getElementById("subtract");
+let multiplyBtn = document.getElementById("multiply");
+let divideBtn = document.getElementById("divide");
 let resultArea = document.getElementById("result");
 
 // function definitions
@@ -31,8 +34,27 @@ function add() {
   let sum = x + y;
   resultArea.innerHTML = sum;
 }
+
+function subtract() {
+  let diff = x - y;
+  resultArea.innerHTML = diff;
+}
+
+function multiply() {
+  let product = x * y;
+  resultArea.innerHTML = product;
+}
+
+function divide() {
+  let division = x / y;
+  resultArea.innerHTML = division;
+}
+
 // event handling
 firstInput.addEventListener("change", setFirstNum);
 secondInput.addEventListener("change", setSecondNum);
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
